0dc0279b429b45f950edfdc6420a128add1dea7e
Bence Fekete <fekete965@gmail.com>
2023-12-05 10:38:18 +0000

    Suggestion about ScrollToTop component

--- client/kohua/src/components/ScrolltoTop.tsx
+++ client/kohua/src/components/ScrolltoTop.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+// This is very weird...
+// This should be a hook or util perhaps, not a component
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
